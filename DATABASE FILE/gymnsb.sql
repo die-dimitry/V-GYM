2039b0d53af4b2588febdb5e5c8c5384cd204948
die-dimitry <yunus787686@gmail.com>
2022-11-02 12:37:00 +0530

    Changed Creds

--- DATABASE FILE/gymnsb.sql
+++ DATABASE FILE/gymnsb.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 02, 2022 at 06:18 PM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
+-- Generation Time: Nov 02, 2022 at 08:04 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -38,7 +39,7 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`user_id`, `username`, `password`, `name`) VALUES
-(2, 'admin', 'f2d0ff370380124029c2b807a924156c', 'admin');
+(2, 'admin', '3cd257ab3cfbf97c950460f2771906d5', 'admin');
 
 -- --------------------------------------------------------
 
@@ -57,10 +58,8 @@ CREATE TABLE `announcements` (
 --
 
 INSERT INTO `announcements` (`id`, `message`, `date`) VALUES
-(7, 'This is to announce that our GYM will remain close for 51 days due to COVID-19.', '2020-03-30'),
-(8, 'Opening of GYM Halls and Clubs are not fixed yet. Stay tuned for more updates!!', '2020-04-03'),
-(9, 'Renovation Going On...', '2020-04-04'),
-(10, 'This is a demo announcement from admin', '2022-06-03');
+(11, 'Happy Guru Nanak Jayanti - On account of Guru Nanak Jayanti, V-Gym will be working from 6am -11am & ', '2022-11-08'),
+(12, 'Happy Halloween \r\n\r\nFrom V-GYM', '2022-11-01');
 
 -- --------------------------------------------------------
 
@@ -99,12 +98,12 @@ CREATE TABLE `equipment` (
 --
 
 INSERT INTO `equipment` (`id`, `name`, `amount`, `quantity`, `vendor`, `description`, `address`, `contact`, `date`) VALUES
-(3, 'Treadmill', 909, 4, 'DnS', 'Edited Description', '7 Cedarstone Drive', '8521479633', '2019-03-07'),
-(4, 'Vertical Press Machine', 949, 3, 'SS Industries', 'For Biceps And Triceps, Upper Back, Chest', '7 Cedarstone Drive', '1245558980', '2020-03-19'),
-(5, 'Dumbell - Adjustable', 102, 26, 'Uptown Suppliers', 'Material: Steel, Rubber Plastic, Concrete', '7 Cedarstone Drive', '9875552100', '2020-03-29'),
-(6, 'Multi Bench Press Machine', 219, 2, 'DnS Suppliers', '6 In 1 Multi Bench With Incline, Flat, Decline Ben', '7 Cedarstone Drive', '7410001010', '2020-04-05'),
-(7, 'Demo', 265, 5, 'Demo', 'This is a demo test.', '77 Demo Lane', '8524445452', '2020-04-03'),
-(10, 'RowWarrior Fitness Rowing Mach', 5616, 12, 'Roww Stores', 'HIGHEST QUALITY: This best of class air rowing mac', '52 Weekley Street', '7412585555', '2021-06-12');
+(3, 'Treadmill', 3636, 4, 'DnS', 'Edited Description', 'Mumbai', '8521479633', '2019-03-07'),
+(4, 'Vertical Press Machine', 949, 3, 'SS Industries', 'For Biceps And Triceps, Upper Back, Chest', 'Nagpur', '1245558980', '2020-03-19'),
+(5, 'Dumbell - Adjustable', 2652, 26, 'Uptown Suppliers', 'Material: Steel, Rubber Plastic, Concrete', 'Mumbai', '9875552100', '2020-03-29'),
+(6, 'Multi Bench Press Machine', 438, 2, 'DnS Suppliers', '6 In 1 Multi Bench With Incline, Flat, Decline Ben', 'Mumbai', '7410001010', '2020-04-05'),
+(7, 'Demo', 1325, 5, 'Demo', 'This is a demo test.', 'Nagpur', '8524445452', '2020-04-03'),
+(10, 'RowWarrior Fitness Rowing Mach', 67392, 12, 'Roww Stores', 'HIGHEST QUALITY: This best of class air rowing mac', 'Pune', '7412585555', '2021-06-12');
 
 -- --------------------------------------------------------
 
@@ -128,12 +127,12 @@ CREATE TABLE `members` (
   `contact` varchar(10) NOT NULL,
   `status` varchar(20) NOT NULL DEFAULT 'Active',
   `attendance_count` int(100) NOT NULL,
-  `ini_weight` int(100) NOT NULL DEFAULT '0',
-  `curr_weight` int(100) NOT NULL DEFAULT '0',
+  `ini_weight` int(100) NOT NULL DEFAULT 0,
+  `curr_weight` int(100) NOT NULL DEFAULT 0,
   `ini_bodytype` varchar(50) NOT NULL,
   `curr_bodytype` varchar(50) NOT NULL,
   `progress_date` date NOT NULL,
-  `reminder` int(11) NOT NULL DEFAULT '0'
+  `reminder` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -141,23 +140,11 @@ CREATE TABLE `members` (
 --
 
 INSERT INTO `members` (`user_id`, `fullname`, `username`, `password`, `gender`, `dor`, `services`, `amount`, `paid_date`, `p_year`, `plan`, `address`, `contact`, `status`, `attendance_count`, `ini_weight`, `curr_weight`, `ini_bodytype`, `curr_bodytype`, `progress_date`, `reminder`) VALUES
-(6, 'Harry Denn', 'harry', 'cac29d7a34687eb14b37068ee4708e7b', 'Male', '2019-12-25', 'Fitness', 165, '2022-06-02', 2021, '3', '64 Mulberry Lane', '8545878545', 'Active', 4, 54, 62, 'Slim', 'Buffed', '2020-04-22', 0),
-(8, 'Charles Anderson', 'charles', 'cac29d7a34687eb14b37068ee4708e7b', 'Male', '2020-01-02', 'Fitness', 55, '2020-04-01', 2020, '3', '99 Heron Way', '8520258520', 'Active', 14, 92, 85, 'Fat', 'Bulked', '2020-04-22', 1),
-(11, 'Justin Schexnayder', 'justin', 'cac29d7a34687eb14b37068ee4708e7b', 'Male', '2019-01-25', 'Cardio', 35, '2020-03-31', 2020, '3', '14 Blair Court', '7535752220', 'Active', 9, 0, 0, '', '', '0000-00-00', 0),
-(14, 'Ryan Crowl', 'ryan', 'cac29d7a34687eb14b37068ee4708e7b', 'Male', '2019-07-13', 'Fitness', 55, '2020-04-02', 2020, '12', '34 Twin Oaks Drive', '1578880010', 'Active', 13, 59, 63, 'Slim', 'Slim', '2020-04-23', 0),
-(16, 'TrialsChanged', 'trials', 'cac29d7a34687eb14b37068ee4708e7b', 'Female', '2020-04-01', 'Fitness', 0, '2021-06-12', 2021, '0', '4 Demo Lane', '741111110', 'Expired', 26, 50, 61, 'Slim', 'Slim', '2021-06-11', 1),
-(17, 'Karen McGray', 'karen', 'cac29d7a34687eb14b37068ee4708e7b', 'Female', '2020-04-02', 'Cardio', 120, '2022-05-31', 2020, '3', '23 Rubaiyat Road', '7441002540', 'Active', 12, 0, 0, '', '', '0000-00-00', 0),
-(18, 'Jeanne Pratt', 'prattj', 'cac29d7a34687eb14b37068ee4708e7b', 'Female', '2020-04-04', 'Fitness', 55, '2021-06-11', 2021, '1', '86 Hilltop Street', '7854445410', 'Active', 11, 0, 0, '', '', '0000-00-00', 0),
-(19, 'George Fann', 'george', 'cac29d7a34687eb14b37068ee4708e7b', 'Male', '2019-04-02', 'Fitness', 55, '2021-06-11', 2021, '1', '43 Oak Drive', '0258987850', 'Active', 22, 0, 0, '', '', '0000-00-00', 1),
-(20, 'Wendy Scott', 'wendy', 'cac29d7a34687eb14b37068ee4708e7b', 'Female', '2020-03-21', 'Fitness', 55, '2021-06-11', 2021, '1', '24 Cody Ridge Road', '8547896520', 'Active', 18, 0, 0, '', '', '0000-00-00', 0),
-(21, 'Patrick Wilson', 'patrick', 'cac29d7a34687eb14b37068ee4708e7b', 'Male', '2020-04-02', 'Cardio', 120, '2022-06-01', 2021, '3', '24 Cody Ridge Road', '9874568520', 'Active', 11, 0, 0, '', '', '0000-00-00', 0),
-(22, 'Tommy Marks', 'tommy', 'cac29d7a34687eb14b37068ee4708e7b', 'Male', '2020-04-01', 'Fitness', 55, '2020-04-05', 2020, '3', '22 Franklin Street', '8529997500', 'Active', 7, 0, 0, '', '', '0000-00-00', 0),
-(23, 'Keith Martin', 'martin', 'cac29d7a34687eb14b37068ee4708e7b', 'Male', '2020-04-02', 'Cardio', 120, '2022-06-02', 2021, '3', '89 Smithfield Avenue', '7895456250', 'Active', 24, 51, 68, 'Slim', 'Muscular', '2022-06-02', 0),
-(24, 'Richard G Langston', 'richard', 'cac29d7a34687eb14b37068ee4708e7b', 'Male', '1990-02-02', 'Sauna', 420, '2022-05-31', 2022, '12', '541  Raoul Wallenber', '7012545580', 'Active', 1, 0, 0, '', '', '0000-00-00', 0),
-(25, 'Raymond Ledesma', 'raymond', 'cac29d7a34687eb14b37068ee4708e7b', 'Male', '1986-02-19', 'Cardio', 480, '2022-06-02', 2022, '12', '2954  Robinson Lane', '4785450002', 'Active', 2, 0, 0, '', '', '0000-00-00', 0),
-(26, 'Mattie F. Maher', 'mattie', 'cac29d7a34687eb14b37068ee4708e7b', 'Female', '1995-05-18', 'Sauna', 420, '2022-06-01', 2022, '12', '73 Settlers Lane', '9995554444', 'Active', 0, 0, 0, '', '', '0000-00-00', 0),
-(27, 'Justin C. Lusk', 'justin', 'cac29d7a34687eb14b37068ee4708e7b', 'Male', '1995-12-12', 'Cardio', 40, '2022-05-30', 2022, '1', '45 Bell Street', '3545785540', 'Active', 1, 0, 0, '', '', '0000-00-00', 0),
-(29, 'Kathy J. Glennon', 'kathy', 'cac29d7a34687eb14b37068ee4708e7b', 'Female', '2022-06-02', 'Fitness', 330, '2022-06-02', 0, '6', '87 Harry Place', '7896587458', 'Active', 0, 0, 0, '', '', '0000-00-00', 0);
+(30, 'Arun', 'arun01', 'd41d8cd98f00b204e9800998ecf8427e', 'Male', '2000-01-01', 'Sauna', 35, '2022-11-02', 2022, '1', '156 The Heights, Ban', '8282828282', 'Active', 1, 0, 0, '', '', '0000-00-00', 0),
+(31, 'Suhail Khan', 'suhail', '3cd257ab3cfbf97c950460f2771906d5', 'Male', '1999-12-03', 'Cardio', 300, '2022-11-01', 2022, '3', '203 Royal Residency,', '7666326641', 'Active', 0, 78, 75, 'Endomorph', 'Endomorph', '2022-11-01', 1),
+(32, 'Aehtisham Geelani', 'aehtisham', '97698cdb39175b74775d750aeb24eed3', 'Male', '1997-09-18', 'Fitness', 720, '2022-11-01', 2022, '6', '561 Rudra Tower, GTB', '9191098761', 'Active', 0, 0, 0, '', '', '0000-00-00', 0),
+(33, 'Joel Fernandes', 'joel01', '39ef50f8c5912fda0ec8ad710da0f9e8', 'Male', '1991-02-14', 'Cardio', 1200, '2022-11-01', 2022, '12', '', '', 'Active', 0, 0, 0, '', '', '0000-00-00', 0),
+(34, 'Sunita Bambale', 'sunita', '270bc4393030fbfb947e9ec35b9c82e1', 'Female', '2002-08-15', 'Cardio', 240, '2022-11-01', 2022, '6', '802 Sunshine Tower, ', '82345689', 'Active', 0, 0, 0, '', '', '0000-00-00', 0);
 
 -- --------------------------------------------------------
 
@@ -228,10 +215,8 @@ CREATE TABLE `staffs` (
 --
 
 INSERT INTO `staffs` (`user_id`, `username`, `password`, `email`, `fullname`, `address`, `designation`, `gender`, `contact`) VALUES
-(1, 'bruno', 'cac29d7a34687eb14b37068ee4708e7b', 'brunoden@mail.com', 'Bruno Den', '26 Morris Street', 'Cashier', 'Male', 852028120),
-(2, 'michelle', 'cac29d7a34687eb14b37068ee4708e7b', 'michelle@mail.com', 'Michelle R. Lane', '61 Stone Lane', 'Trainer', 'Female', 2147483647),
-(3, 'james', 'cac29d7a34687eb14b37068ee4708e7b', 'jamesb@mail.com', 'James Brown', '12 Deer Ridge Drive', 'Trainer', 'Male', 2147483647),
-(4, 'bruce', 'cac29d7a34687eb14b37068ee4708e7b', 'bruce@mail.com', 'Bruce H. Klaus', '68 Lake Floyd Circle', 'Manager', 'Male', 1458887788);
+(5, 'nikhil', '350d89c1cd6592bbbd1ed2e8a4f3ddba', 'nikhil.dhangada@vgym.com', 'Nikhil Dhangada', '102, CHS Ltd, Chunab', 'Trainer', 'Male', 2147483647),
+(6, 'pooja', '9cbb6aebcf5ae14a9248b4c08165212e', 'pooja.jaiswal@vgym.com', 'Pooja Singh', '409, Orchids, Wdala', 'Front Desk Staff', 'Female', 2147483647);
 
 -- --------------------------------------------------------
 
@@ -256,8 +241,7 @@ INSERT INTO `todo` (`id`, `task_status`, `task_desc`, `user_id`) VALUES
 (22, 'In Progress', 'Standing Workouts For Flat Abs', 6),
 (23, 'In Progress', 'Triceps Buildup - 3 set', 14),
 (24, 'Pending', 'Decline dumbbell bench press', 6),
-(27, 'Pending', 'dddd', 0),
-(28, 'In Progress', 'Test 1', 23);
+(30, 'In Progress', '60 Push Ups in 3 sets', 31);
 
 --
 -- Indexes for dumped tables
@@ -326,46 +310,56 @@ ALTER TABLE `todo`
 --
 ALTER TABLE `admin`
   MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
 -- AUTO_INCREMENT for table `announcements`
 --
 ALTER TABLE `announcements`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
 --
 -- AUTO_INCREMENT for table `attendance`
 --
 ALTER TABLE `attendance`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
 --
 -- AUTO_INCREMENT for table `equipment`
 --
 ALTER TABLE `equipment`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
 --
 -- AUTO_INCREMENT for table `members`
 --
 ALTER TABLE `members`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+
 --
 -- AUTO_INCREMENT for table `rates`
 --
 ALTER TABLE `rates`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
 -- AUTO_INCREMENT for table `reminder`
 --
 ALTER TABLE `reminder`
   MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
 --
 -- AUTO_INCREMENT for table `staffs`
 --
 ALTER TABLE `staffs`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
 --
 -- AUTO_INCREMENT for table `todo`
 --
 ALTER TABLE `todo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
